afdfcebf57aa01874d4a144ae59d45daea344b17
marchiba99 <marchiba99@gmail.com>
2021-10-21 13:12:02 +0200

    first

---
+++ dist/bundles/esm.js
@@ -0,0 +1,19 @@
+/**
+ * Returns the a Hello to the input string name
+ *
+ * @remarks An example function that runs different code in Node and Browser javascript
+ *
+ * @param name - The name to say hello to
+ *
+ * @returns A gratifying Hello to the input name
+ */
+function helloWorld(name) {
+    const text = `Hello ${name}!`;
+    {
+        console.log(`Browser says "${text}"`);
+    }
+    return text;
+}
+
+export { helloWorld };
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXNtLmpzIiwic291cmNlcyI6WyIuLi8uLi9zcmMvdHMvaGVsbG8td29ybGQudHMiXSwic291cmNlc0NvbnRlbnQiOm51bGwsIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBOzs7Ozs7Ozs7U0FTZ0IsVUFBVSxDQUFFLElBQVk7SUFDdEMsTUFBTSxJQUFJLEdBQUcsU0FBUyxJQUFJLEdBQUcsQ0FBQTtJQUNiO1FBQ2QsT0FBTyxDQUFDLEdBQUcsQ0FBQyxpQkFBaUIsSUFBSSxHQUFHLENBQUMsQ0FBQTtLQUd0QztJQUNELE9BQU8sSUFBSSxDQUFBO0FBQ2I7Ozs7In0=
